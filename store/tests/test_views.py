cc1763278de832f22155f3879d440c2f17d39232
klimrus61 <klimrus61@gmail.com>
2022-07-13 09:07:30 +0300

    basket-function + test

--- store/tests/test_views.py
+++ store/tests/test_views.py
@@ -1,22 +1,18 @@
-# from unittest import skip
+from importlib import import_module
+
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.http import HttpRequest
-from django.test import Client, RequestFactory, TestCase
+from django.test import Client, TestCase
 from django.urls import reverse
 
 from store.models import Category, Product
 from store.views import product_all
 
-# @skip('demonstrating skipping')
-# class TestSkip(TestCase):
-#    def test_skip_exmaple(self):
-#        pass
-
 
 class TestViewResponses(TestCase):
     def setUp(self):
         self.c = Client()
-        self.factory = RequestFactory()
         Category.objects.create(name='django', slug='django')
         User.objects.create(username='klim')
         self.data1 = Product.objects.create(category_id=1, title='django beginners', created_by_id=1,
@@ -47,17 +43,10 @@ class TestViewResponses(TestCase):
 
     def test_homepage_html(self):
         request = HttpRequest()
+        engine = import_module(settings.SESSION_ENGINE)
+        request.session = engine.SessionStore()
         response = product_all(request)
         html = response.content.decode('utf8')
-        print(html)
-        self.assertIn('<title>ArtStore</title>', html)
-        self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_view_function(self):
-        request = self.factory.get('/django-beginners')
-        response = product_all(request)
-        html = response.content.decode('utf8')
-        self.assertIn('<title>ArtStore</title>', html)
+        # self.assertIn('<title>ArtStore</title>', html)
         self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
         self.assertEqual(response.status_code, 200)
